7ffd071a405686ac71115a97e2fd6d22c261d76a
fareldev <ffarelscho@gmail.com>
2025-11-26 10:17:35 +0700

    Modify Percobaan 4 - PengunjungCafe08

--- PengunjungCafe08.java
+++ PengunjungCafe08.java
@@ -1,8 +1,12 @@
 public class PengunjungCafe08 {
     static void daftarPengunjung(String ...namaPengunjung) {
         System.out.print("Daftar Pengunjung Cafe: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        // for (int i = 0; i < namaPengunjung.length; i++) {
+        //     System.out.println("- " + namaPengunjung[i]);
+        // }
+
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 
