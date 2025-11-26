5c18c6055526f74de6dce99d1889f4bcb59538dd
fareldev <ffarelscho@gmail.com>
2025-11-26 08:33:26 +0700

    Add Percobaan 1 - Kafe08

---
+++ Kafe08.java
@@ -0,0 +1,23 @@
+public class Kafe08 {
+    // Percobaan 1: Membuat fungsi menu()
+    public static void menu() {
+        System.out.println("=== Kafe Menu ===");
+        System.out.println("1. Kopi Hitam - Rp 15000");
+        System.out.println("2. Cappucino - Rp 20000");
+        System.out.println("3. Latte - Rp 22000");
+        System.out.println("4. Teh Tarik - Rp 12000");
+    }
+
+    public static void main(String[] args) {
+        menu();
+
+        // Menampilkan menu tanpa fungsi
+        System.out.println("\n=== Kafe Menu (Tanpa Fungsi) ===");
+        System.out.println("1. Kopi Hitam - Rp 15000");
+        System.out.println("2. Cappucino - Rp 20000");
+        System.out.println("3. Latte - Rp 22000");
+        System.out.println("4. Teh Tarik - Rp 12000");
+
+        // Jadi langsung saja tanpa perlu untuk memanggil fungsi menu() atau tanpa fungsi menu()
+    }
+}
