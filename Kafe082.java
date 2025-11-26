c660a04c90639263bb6b3adfcd49d5fbdb84d16d
fareldev <ffarelscho@gmail.com>
2025-11-26 09:04:27 +0700

    Modify Percobaan 2 - Kafe082

--- Kafe082.java
+++ Kafe082.java
@@ -1,11 +1,20 @@
 public class Kafe082 {
     // Percobaan 2: Membuat fungsi menu() dengan parameter
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember,  String kodePromo) { // Penambahan parameter kodePromo
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda mendapatkan diskon khusus 10% untuk member!");
         } 
+
+        // Penambahan kode promo
+        if (kodePromo.equalsIgnoreCase("DISKON50%")) {
+            System.out.println("Anda mendapatkan diskon tambahan 50% dengan kode promo!");
+        } else if (kodePromo.equalsIgnoreCase("DISKON30%")) {
+            System.out.println("Anda mendapatkan diskon tambahan 20% dengan kode promo!");
+        } else {
+            System.out.println("Kode Promo Invalid.");
+        }
 
         System.out.println("=== Kafe Menu ===");
         System.out.println("1. Kopi Hitam - Rp 15000");
@@ -15,6 +24,8 @@ public class Kafe082 {
     }
 
     public static void main(String[] args) {
-        menu("Andi", true);
+        menu("Andi", true, "DISKON50%");
+        menu("Budi", false, "DISKON30%");
+        menu("Citra", true, "DISKON10%"); // Kode promo invalid
     }
 }
